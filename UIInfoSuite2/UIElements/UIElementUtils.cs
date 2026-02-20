a161a5d6a744177fb61522ba40f4c35d70884ca4
538Michael <michael.maicom0@gmail.com>
2026-02-20 00:03:44 -0300

    perf(IsRenderingNormally): replace array + LINQ with direct boolean chain
    Replace bool[] allocation and .All(lambda) with a short-circuit &&
    chain to eliminate the array, iterator, and delegate allocation on
    every frame. This method is called multiple times per frame by nearly
    every feature in the mod.

--- UIInfoSuite2/UIElements/UIElementUtils.cs
+++ UIInfoSuite2/UIElements/UIElementUtils.cs
@@ -1,4 +1,3 @@
-﻿using System.Linq;
 using StardewValley;
 
 namespace UIInfoSuite2.UIElements;
@@ -7,16 +6,11 @@ public static class UIElementUtils
 {
   public static bool IsRenderingNormally()
   {
-    bool[] conditions =
-    {
-      !Game1.game1.takingMapScreenshot,
-      !Game1.eventUp,
-      !Game1.viewportFreeze,
-      !Game1.freezeControls,
-      Game1.viewportHold <= 0,
-      Game1.displayHUD
-    };
-
-    return conditions.All(condition => condition);
+    return !Game1.game1.takingMapScreenshot &&
+           !Game1.eventUp &&
+           !Game1.viewportFreeze &&
+           !Game1.freezeControls &&
+           Game1.viewportHold <= 0 &&
+           Game1.displayHUD;
   }
 }
